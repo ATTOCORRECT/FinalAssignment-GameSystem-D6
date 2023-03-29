9e8fba09f94227f95b6f84d9678f03b3bd028ebe
Auto <97545662+ATTOCORRECT@users.noreply.github.com>
2023-03-29 15:24:53 -0400

    Added Game Scene & Dice

---
+++ D6-Game/Assets/Scripts/DicePositionManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DicePositionManager : MonoBehaviour
+{
+    public GameObject Die;
+
+    GameObject[] DicePositionEmpties;
+    Vector2[] dicePositionLUT = new Vector2[16];
+
+    List<GameObject> Dice = new List<GameObject>(16);
+
+    void Start()
+    {
+        
+        DicePositionEmpties = GameObject.FindGameObjectsWithTag("DicePosition");
+        for (int i = 0; i < DicePositionEmpties.Length; i++)
+        {
+            dicePositionLUT[i] = DicePositionEmpties[i].transform.position;
+        }
+
+        for (int i = 0; i < 15; i++)
+        {
+            Dice.Add(Instantiate(Die, dicePositionLUT[i], Quaternion.identity));
+        }
+
+        Dice.Add(null);
+
+
+    }
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < 16; i++)
+        {
+            if (Dice[i] != null)
+            {
+                Dice[i].transform.position = Vector2.Lerp(Dice[i].transform.position, dicePositionLUT[i], 0.2f);
+            }
+        }
+    }
+
+    public void SelectDie(int column) {
+        Object.Destroy(Dice[15], 0);
+        Dice[15] = Dice[column + 10];
+        Dice[column + 10] = Dice[column + 5];
+        Dice[column + 5] = Dice[column];
+        Dice[column] = (Instantiate(Die, dicePositionLUT[column], Quaternion.identity));
+    }
+    
+
+}
---
+++ D6-Game/Assets/Scripts/DieManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieManager : MonoBehaviour
+{
+    public Color[] dieColors;
+    public Color[] numberColors;
+
+    public bool UseArabic = false;
+
+    public Sprite[] numberSprites;
+
+    public GameObject Square;
+    public GameObject Number;
+
+    int colorIndex;
+    int numberIndex;
+
+    
+
+    void Start()
+    {
+        colorIndex = Random.Range(0, dieColors.Length);
+        numberIndex = Random.Range(0, 6);
+
+        if (UseArabic) {
+            numberIndex += 6;
+        }
+
+        Square.GetComponent<SpriteRenderer>().color = dieColors[colorIndex];
+        Number.GetComponent<SpriteRenderer>().color = numberColors[colorIndex];
+        Number.GetComponent<SpriteRenderer>().sprite = numberSprites[numberIndex];
+    }
+
+}
